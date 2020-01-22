fcfae380369cc7d4caae339223c22c0c8a8168eb
ringo2455 <iby2455@gmail.com>
2020-01-22 22:26:50 +0900

    Redux moduel refactoring using typesafe-actions

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,25 +1,16 @@
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECREASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
+import { deprecated, ActionType, createReducer } from 'typesafe-actions';
+const { createStandardAction } = deprecated;
 
-export const increase = () => ({
-  type: INCREASE
-});
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+const INCREASE_BY = 'counter/INCREASE_BY';
 
-export const decrease = () => ({
-  type: DECREASE
-});
+export const increase = createStandardAction(INCREASE)();
+export const decrease = createStandardAction(DECREASE)();
+export const increaseBy = createStandardAction(INCREASE_BY)<number>();
 
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  payload: diff
-});
-
-// Reducer에서 action 파라미터 쓸 때 type을 지정하기 위해 선언
-type CounterAction =
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseBy>;
+const actions = { increase, decrease, increaseBy };
+type CounterAction = ActionType<typeof actions>;
 
 type CounterState = {
   count: number;
@@ -29,19 +20,10 @@ const initialState: CounterState = {
   count: 0
 };
 
-function Counter(state: CounterState = initialState, action: CounterAction) {
-  switch (action.type) {
-    case INCREASE:
-      return { count: state.count + 1 };
-    case DECREASE:
-      return { count: state.count - 1 };
-    case INCREASE_BY:
-      return {
-        count: state.count + action.payload
-      };
-    default:
-      return state;
-  }
-}
+const counter = createReducer<CounterState, CounterAction>(initialState, {
+  [INCREASE]: state => ({ count: state.count + 1 }),
+  [DECREASE]: state => ({ count: state.count - 1 }),
+  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload })
+});
 
-export default Counter;
+export default counter;
